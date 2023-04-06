e36e46fd87caf5f7f1101814e24d1cef541de2f8
amananandsingh <amananandsingh>
2023-04-06 22:39:03 +0530

    jjki

---
+++ daily java progs/hibib.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hibib {
+  public static void main(String[] args) {
+       //  Scanner sch =new Scanner(System.in);//take input method;
+       // int n=sc.nextInt();//take input integer;
+        //char hh=sc.next().charAt(0);//take input character
+        //float uv=sc.next().charAt();
+       // float myFloat = sch.nextFloat();//real way of taking float input
+       char a='A';
+       //++a;
+       a++;
+System.out.println(a);
+       // for(int i=0;i<=10;i++){
+         //System.out.println(i*n+"ak47");
+         //System.out.print(hh);
+      //   System.out.println(myFloat);
+        }     
+   //}    
+}
+
